7fa095254fed879d6ff0a337a7f709e5d5114d30
codejst <jste223@g.uky.edu>
2018-09-30 21:42:40 -0400

    Add the second page with a list of medals

---
+++ OneHourWalker/MedalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MedalViewController.swift
+//  OneHourWalker
+//
+//  Created by Terrell, Joshua on 9/30/18.
+//  Copyright © 2018 Matt Maher. All rights reserved.
+//
+
+import UIKit
+
+class MedalViewController: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBOutlet weak var MedalLabel: UILabel!
+    @IBOutlet weak var Medal2Label: UILabel!
+    @IBOutlet weak var Medal3Label: UILabel!
+    
+    var myString: String = "Unlocked"
+    
+    var num: Int = 10
+    
+
+}
